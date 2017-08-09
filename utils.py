2dbda00d9b82c52472b6ac68bd5b9660004c6e2f
Joseph Palermo <josephwpalermo@gmail.com>
2017-08-09 15:20:36 -0400

    cleanup utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,6 @@
 import pickle
 import numpy as np
 
-def softmax(x):
-    xt = np.exp(x - np.max(x))
-    return xt / np.sum(xt)
-
 def pp_output(a, index_to_word):
     """Convert an array of indices into a string containing the corresponding
     words."""
