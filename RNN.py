be59a61de6798db42465ca253beefa3f1419ced7
Joseph Palermo <josephwpalermo@gmail.com>
2017-08-09 15:20:19 -0400

    add core RNN functionality

--- RNN.py
+++ RNN.py
@@ -1,102 +1,92 @@
-import math
+"""This code is inspired by an RNN implementation by Danny Britz:
+https://github.com/dennybritz/rnn-tutorial-rnnlm/
+"""
+
 import numpy as np
-import theano
+import theano as theano
 import theano.tensor as T
-from theano.tensor import tanh
-from theano.tensor.nnet import softmax
 
 class RNN:
 
-    def __init__(self, vocab_size, state_size):
+    def __init__(self, vocab_size, state_size, bptt_truncate,
+                 model_params=None):
         self.vocab_size = vocab_size
         self.state_size = state_size
-        # x is an input, y is the corresponding label
-        self.x = T.matrix("x")
-        self.y = T.matrix("y")
-        # s is the state matrix
-        # todo, 1 -> mini_batch_size?
-        self.s = theano.shared(np.zeros((state_size, 1), dtype=theano.config.floatX),
-                               name="s",
-                               borrow=True)
-        # U maps an input element into an array of size state_size
-        u_shape = (state_size, vocab_size)
-        uniform_u = np.array(np.random.uniform(low=-1/math.sqrt(vocab_size),
-                                               high=1/math.sqrt(vocab_size),
-                                               size=u_shape),
-                             dtype=theano.config.floatX)
-        self.u = theano.shared(uniform_u, name="u", borrow=True)
-        # W maps an state element into an array of the same size
-        w_shape = (state_size, state_size)
-        uniform_w = np.array(np.random.uniform(low=-1/math.sqrt(state_size),
-                                               high=1/math.sqrt(state_size),
-                                               size=w_shape),
-                             dtype=theano.config.floatX)
-        self.w = theano.shared(uniform_w, name="w", borrow=True)
-        # V maps an element of state size into an array of size vocab_size
-        v_shape = (vocab_size, state_size)
-        uniform_v = np.array(np.random.uniform(low=-1/math.sqrt(state_size),
-                                               high=1/math.sqrt(state_size),
-                                               size=v_shape),
-                             dtype=theano.config.floatX)
-        self.v = theano.shared(uniform_v, name="v", borrow=True)
-        self.params = [self.U, self.W, self.V]
-        # state updates are constructed from the input and the previous state
-        self.update_state = (self.s, tanh(T.dot(self.u, self.x) + T.dot(self.w, self.s)))
-        # the output is constructed from the state
-        self.pre_o = T.dot(self.v, self.s)
-        self.o = softmax(T.dot(self.v, self.s))
+        self.bptt_truncate = bptt_truncate
 
+        # define inputs to the computation graph
+        x = T.ivector('x')
+        y = T.ivector('y')
+        eta = T.scalar('eta') # eta is the learning rate
 
+        # if there are pre-existing model parameters, use them
+        if model_params:
+            U, W, V = model_params['U'], model_params['W'], model_params['V']
+        else:
+            print("initializing model weights")
+            U = np.random.uniform(-np.sqrt(1/vocab_size), np.sqrt(1/vocab_size), (state_size, vocab_size))
+            V = np.random.uniform(-np.sqrt(1/state_size), np.sqrt(1/state_size), (vocab_size, state_size))
+            W = np.random.uniform(-np.sqrt(1/state_size), np.sqrt(1/state_size), (state_size, state_size))
+        self.U = theano.shared(name='U', value=U.astype(theano.config.floatX))
+        self.V = theano.shared(name='V', value=V.astype(theano.config.floatX))
+        self.W = theano.shared(name='W', value=W.astype(theano.config.floatX))
+        self.params = [self.U, self.V, self.W]
 
-    # convert a list of indices into a matrix of one-hot vectors
-    # i.e. process a single sentence as an input matrix
-    def process_input(self, inpt):
-        num_time_steps = len(inpt)
-        a = np.zeros((self.vocab_size, num_time_steps))
-        for i, num in enumerate(inpt):
-            a[num][i] = 1
-        return a
+        # build recurrence into the computation graph
+        def layer_step(x_t, s_t_prev, U, V, W):
+            s_t = T.tanh(U[:,x_t] + W.dot(s_t_prev))
+            o_t = T.nnet.softmax(V.dot(s_t))
+            return [o_t[0], s_t]
+        [outputs, states], updates = theano.scan(
+            fn=layer_step,
+            sequences=x,
+            outputs_info=[None, dict(initial=T.zeros(self.state_size))],
+            non_sequences=[self.U, self.V, self.W],
+            truncate_gradient=self.bptt_truncate)
 
-    # convert a matrix of one-hot vectors into an array of indices
-    def process_output(self, output):
-        output_list = []
-        for i in output:
-            output_list.append(np.argmax(output[i]))
-        return np.array(output_list)
+        # define outputs of the computation graph
+        prediction = T.argmax(outputs, axis=1)
+        cost = T.sum(T.nnet.categorical_crossentropy(outputs, y))
+        grads = T.grad(cost, self.params)
+        gradient_descent_updates = [(param, param - eta * grad) \
+                                    for param, grad in zip(self.params, grads)]
 
-    def sgd(self, x_train, y_train, learning_rate, num_epochs):
+        # define functions for computing results and training
+        self.propagate = theano.function([x], outputs)
+        self.predict = theano.function([x], prediction)
+        self.get_cost = theano.function([x, y], cost)
+        self.train_on_example = theano.function(inputs=[x,y,eta],
+                                                updates=gradient_descent_updates)
 
-        cost = ...
-        grad = ...
-        update_parameters = [(param, param - learning_rate * grad)
-                            for param in self.params for grad in grads]
+    def compute_cost(self, xs, ys):
+        """Compute the cost with respect to a collection of data"""
+        cumulative_cost = np.sum([self.get_cost(x,y) for x,y in zip(xs, ys)])
+        num_examples = np.sum([len(y) for y in ys])
+        return cumulative_cost/num_examples
 
-        self.train = theano.function(x, y, learning_rate, updates=[update_parameters]):
-
-        # actually perform training
+    def sgd(self, training_data, num_epochs, learning_rate,
+            validation_data=None, test_data=None):
+        """Perform stochastic gradient descent."""
+        x_train, y_train = training_data
+        if validation_data:
+            x_validation, y_validation = validation_data
         num_examples = len(x_train)
         for epoch_i in range(num_epochs):
             # shuffle the training data
             perm = np.arange(num_examples)
             np.random.shuffle(perm)
-            self.x_train = self.x_train[perm]
-            self.y_train = self.y_train[perm]
+            x_train = x_train[perm]
+            y_train = y_train[perm]
+            print("training epoch {}".format(epoch_i))
+            # update parameters once for each training example
+            for i in range(num_examples):
+                self.train_on_example(x_train[i],y_train[i],learning_rate)
+            # compute post-epoch validation cost
+            if validation_data:
+                validation_cost = self.compute_cost(x_validation, y_validation)
+                print("validation cost at epoch {} is: {}".format(epoch_i, validation_cost))
 
-
-
-
-
-    def predict(self, inpt):
-        # construct theano function for prediction
-        predict_ = theano.function([self.x], [self.o, self.pre_o])
-
-        # test
-        print(inpt)
-        inpt_mat = self.process_input(inpt)
-        print(inpt_mat)
-        inpt_vector = inpt_mat[:,0]
-        inpt_vector = np.reshape(inpt_vector, (15, 1))
-        print(inpt_vector.shape)
-        output_mat = predict_(inpt_vector)
-        print(output_mat)
-        # print(self.process_output(output_mat))
+    def get_predictions(self, inputs):
+        """Produce outputs for a set of inputs. Parameter inputs are represented
+        as a list of indices."""
+        return [self.predict(x) for x in inputs]
